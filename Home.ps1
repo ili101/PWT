0390b8f56137086e451c4aa29052610d9bde2434
ili101 <zoby101@gmail.com>
2020-12-31 23:28:46 +0200

    Pode.Web layouts test

--- Home.ps1
+++ Home.ps1
@@ -9,73 +9,70 @@ Start-PodeServer {
     if ($Config['Login']) {
         . $Config['Login']
     }
+    
+    New-PodeLoggingMethod -File -Name 'Errors' | Enable-PodeErrorLogging -Levels @('Error', 'Warning', 'Informational', 'Verbose', 'Debug')
+    New-PodeLoggingMethod -File -Name 'Requests' | Enable-PodeRequestLogging
 
-    $ResultsTable = New-PodeWebTable -Name 'Results' -Id 'TableResults' -Filter
-    $DownloadSection = New-PodeWebSection -Name 'Download Section' -Elements @(
-        New-PodeWebButton -Name 'Download' -Id 'DownloadResults' -Icon 'Download' -ScriptBlock {
-            Lock-PodeObject -Object $WebEvent.Lockable {
-                $Results = Get-PodeState -Name 'Results'
+    $DownloadSection = New-PodeWebCard -Name 'Download Section' -Content @(
+        New-PodeWebForm -Name 'Search' -ArgumentList ($Config['Dummy'], $Config['Debug'], $Config['Exchange']) -ScriptBlock {
+            param (
+                $Dummy,
+                $Debug,
+                $Exchange
+            )
+            $ErrorActionPreference = 'Stop'
+            try {
+                function Format-Exchange {
+                    [CmdletBinding()]
+                    param (
+                        [Parameter(ValueFromPipeline)]
+                        $InputObject
+                    )
+                    process {
+                        $InputObject | Select-Object -Property @{N = 'Timestamp' ; E = { $_.Timestamp.ToString() } }, 'EventId', 'Source', 'Sender', 'Recipients', 'MessageSubject'
+                    }
+                }
+                # Clear Parameters.
+                $InputData = @{}
+                foreach ($Param in $WebEvent.Data.GetEnumerator()) {
+                    if (![string]::IsNullOrWhiteSpace($Param.Value)) {
+                        $InputData[$Param.Key] = $Param.Value
+                    }
+                }
+                Import-Module -Name (Join-Path $PSScriptRoot 'EXLogLib.psm1')
+                Connect-Exchange @Exchange
+                $global:Results = Search-MessageTracking @InputData
+                Show-PodeWebToast -Message "Found $($Results.Length) results"
+                $Results | Format-Exchange | Out-PodeWebTable -Id 'TableResults'
+
+                # if ($Debug) {
+                #     $Results | Out-PodeWebTextbox -Multiline -Preformat
+                # }
             }
-            Export-Excel -InputObject $Results['Data'] -WorksheetName 'Log' -TableName 'Log' -AutoSize -Path 'C:\Temp\test.xlsx'
+            catch {
+                if ($Debug) {
+                    $ErrorMsg = if ($PSVersionTable.PSVersion.Major -gt 5) {
+                        $_ | Get-Error
+                    }
+                    else {
+                        $_
+                    }
+                    $ErrorMsg | Out-PodeWebTextbox -Multiline -Preformat
+                }
+            }
+        } -Content @(
+            New-PodeWebTextbox -Name 'Start' -Type Date
+            New-PodeWebTextbox -Name 'End' -Type Date
+            New-PodeWebTextbox -Name 'Sender' -Type Email
+            New-PodeWebTextbox -Name 'Recipients' -Type Email
+            New-PodeWebTextbox -Name 'MessageSubject'
+        )
+        New-PodeWebButton -Name 'Download' -Id 'DownloadResults' -Icon 'Download' -ScriptBlock {
+            Write-Warning 'Button run'
+            Export-Excel -InputObject $global:Results -WorksheetName 'Log' -TableName 'Log' -AutoSize -Path 'C:\Temp\test.xlsx'
         }
         New-PodeWebLink -Source 'https://docs.microsoft.com/en-us/exchange/mail-flow/transport-logs/message-tracking?view=exchserver-2019#event-types-in-the-message-tracking-log' -Value 'Event types in the message tracking log' -NewTab
+        New-PodeWebTable -Name 'Results' -Id 'TableResults' -Filter
     )
-
-    $Form = New-PodeWebForm -Name 'Search' -ArgumentList ($Config['Dummy'], $Config['Debug'], $Config['Exchange']) -ScriptBlock {
-        param (
-            $Dummy,
-            $Debug,
-            $Exchange
-        )
-        $ErrorActionPreference = 'Stop'
-        try {
-            function Format-Exchange {
-                [CmdletBinding()]
-                param (
-                    [Parameter(ValueFromPipeline)]
-                    $InputObject
-                )
-                process {
-                    $InputObject | Select-Object -Property @{N = 'Timestamp' ; E = { $_.Timestamp.ToString() } }, 'EventId', 'Source', 'Sender', 'Recipients', 'MessageSubject'
-                }
-            }
-            # Clear Parameters.
-            $InputData = @{}
-            foreach ($Param in $WebEvent.Data.GetEnumerator()) {
-                if (![string]::IsNullOrWhiteSpace($Param.Value)) {
-                    $InputData[$Param.Key] = $Param.Value
-                }
-            }
-            Import-Module -Name (Join-Path $PSScriptRoot 'EXLogLib.psm1')
-            Connect-Exchange @Exchange
-            $Results = Search-MessageTracking @InputData
-            Show-PodeWebToast -Message "Found $($Results.Length) results"
-            $Results | Format-Exchange | Out-PodeWebTable -Id 'TableResults'
-            Lock-PodeObject -Object $WebEvent.Lockable {
-                $null = Set-PodeState -Name 'Results' -Value @{ 'Data' = $Results }
-            }
-
-            # if ($Debug) {
-            #     $Results | Out-PodeWebTextbox -Multiline -Preformat
-            # }
-        }
-        catch {
-            if ($Debug) {
-                $ErrorMsg = if ($PSVersionTable.PSVersion.Major -gt 5) {
-                    $_ | Get-Error
-                }
-                else {
-                    $_
-                }
-                $ErrorMsg | Out-PodeWebTextbox -Multiline -Preformat
-            }
-        }
-    } -Elements @(
-        New-PodeWebTextbox -Name 'Start' -Type Date
-        New-PodeWebTextbox -Name 'End' -Type Date
-        New-PodeWebTextbox -Name 'Sender' -Type Email
-        New-PodeWebTextbox -Name 'Recipients' -Type Email
-        New-PodeWebTextbox -Name 'MessageSubject'
-    )
-    Add-PodeWebPage -Name 'Message Tracking' -Icon Activity -Components $Form, $DownloadSection, $ResultsTable
+    Add-PodeWebPage -Name 'Message Tracking' -Icon Activity -Layouts $DownloadSection
 }
